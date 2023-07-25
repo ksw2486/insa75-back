c0067e8a0253900de2c20eef041d86a63a916a3a
Hxexx <hxexx2017@gmail.com>
2023-07-25 09:44:06 +0900

    사원조회 수정

--- src/main/java/kr/co/seoulit/insa/empmgmtsvc/empinfomgmt/controller/EmpDetailController.java
+++ src/main/java/kr/co/seoulit/insa/empmgmtsvc/empinfomgmt/controller/EmpDetailController.java
@@ -20,33 +20,33 @@ import kr.co.seoulit.insa.sys.mapper.DatasetBeanMapper;
 @RequestMapping("/empinfomgmt/*")
 @RestController
 public class EmpDetailController {
-	
+
 	@Autowired
 	private EmpInfoService empInfoService;
 
 	@Autowired
 	private DatasetBeanMapper datasetBeanMapper;
-	
+
 	@RequestMapping("/empdetail/all")
 	public void findAllEmployeeInfo(@RequestAttribute("reqData") PlatformData reqData,
 								@RequestAttribute("resData") PlatformData resData) throws Exception{
-		
+
 
 		String empCode = reqData.getVariable("empCode").getString();
-		
+
 			EmpTO empTO=empInfoService.findAllEmpInfo(empCode);
 			ArrayList<WorkInfoTO> workInfoTO = empTO.getWorkInfo();
 			empTO.setHobong(workInfoTO.get(0).getHobong());
-			ArrayList<LicenseInfoTO> licenseInfoTO = empTO.getLicenseInfoList();			
+			ArrayList<LicenseInfoTO> licenseInfoTO = empTO.getLicenseInfoList();
 			ArrayList<FamilyInfoTO> familyInfoTO = empTO.getFamilyInfoList();
-			
+
 			datasetBeanMapper.beanToDataset(resData,empTO,EmpTO.class);
 			datasetBeanMapper.beansToDataset(resData,workInfoTO,WorkInfoTO.class);
 			datasetBeanMapper.beansToDataset(resData,familyInfoTO,FamilyInfoTO.class);
 			datasetBeanMapper.beansToDataset(resData,licenseInfoTO,LicenseInfoTO.class);
 
 	}
-	
+
 	@RequestMapping("/empdetail/empcode")
 	public void modifyEmployee(@RequestAttribute("reqData") PlatformData reqData,
 			@RequestAttribute("resData") PlatformData resData) throws Exception{
@@ -54,22 +54,22 @@ public class EmpDetailController {
 		System.out.println("<<<<<<<<<<<<<<<<<<<<사원상세조회 저장 컨트롤러 진입 >>>>>>>>>>>>>>>>>>>>");
 		EmpTO emp = datasetBeanMapper.datasetToBean(reqData, EmpTO.class);
 		System.out.println("//////////////////////////////"+emp.toString());
-		ArrayList<WorkInfoTO> workinfo = (ArrayList<WorkInfoTO>) datasetBeanMapper.datasetToBeans(reqData, WorkInfoTO.class);
+		/*ArrayList<WorkInfoTO> workinfo = (ArrayList<WorkInfoTO>) datasetBeanMapper.datasetToBeans(reqData, WorkInfoTO.class);
 		ArrayList<FamilyInfoTO> familyinfo = (ArrayList<FamilyInfoTO>) datasetBeanMapper.datasetToBeans(reqData, FamilyInfoTO.class);
 		ArrayList<LicenseInfoTO> licenseinfo = (ArrayList<LicenseInfoTO>) datasetBeanMapper.datasetToBeans(reqData, LicenseInfoTO.class);
 
-		
+
 		emp.setWorkInfo(workinfo);
 		emp.setFamilyInfoList(familyinfo);
-		emp.setLicenseInfoList(licenseinfo);
-		
+		emp.setLicenseInfoList(licenseinfo);*/
+
 			empInfoService.modifyEmployee(emp);
 
 	}
-	
+
 	@RequestMapping("/empdetail/empcode2")
 	public void removeEmployeeList(@RequestAttribute("reqData") PlatformData reqData,
 						@RequestAttribute("resData") PlatformData resData) throws Exception{
-		
+
 	}
 }
